860a4d03df7e5f821f4b4114b406a8e128a19b64
Fernando Mors <>
2023-10-21 23:48:37 -0400

    Refatorei a tela de register

--- PetID/PetID/Screens/Register/RegisterViewController.swift
+++ PetID/PetID/Screens/Register/RegisterViewController.swift
@@ -10,21 +10,13 @@ import UIKit
 class RegisterViewController: UIViewController {
     
     @IBOutlet weak var backButton: UIButton!
-    
     @IBOutlet weak var logoImage: UIImageView!
-    
     @IBOutlet weak var createAccountLabel: UILabel!
-    
     @IBOutlet weak var nameTextField: UITextField!
-    
     @IBOutlet weak var lastNameTextField: UITextField!
-    
     @IBOutlet weak var emailTextField: UITextField!
-    
     @IBOutlet weak var passwordTextField: UITextField!
-    
     @IBOutlet weak var confirmPasswordTextField: UITextField!
-    
     @IBOutlet weak var createAccountButton: UIButton!
     
     override func viewDidLoad() {
@@ -46,32 +38,22 @@ class RegisterViewController: UIViewController {
         confirmPasswordTextField.delegate = self
     }
     
+    private func textFieldPattern(TextField: UITextField, Placeholder: String, Radius: CGFloat, maskToBounds: Bool) {
+        TextField.placeholder = Placeholder
+        TextField.layer.cornerRadius = Radius
+        TextField.layer.masksToBounds = maskToBounds
+    }
+    
     func configTextField() {
-        
-        nameTextField.placeholder = "Nome"
-        nameTextField.layer.cornerRadius = 10
-        nameTextField.layer.masksToBounds = true
-        
-        lastNameTextField.placeholder = "Sobrenome"
-        lastNameTextField.layer.cornerRadius = 10
-        lastNameTextField.layer.masksToBounds = true
-        
-        emailTextField.placeholder = "Email"
-        emailTextField.layer.cornerRadius = 10
-        emailTextField.layer.masksToBounds = true
-        
-        passwordTextField.placeholder = "Senha"
-        passwordTextField.layer.cornerRadius = 10
-        passwordTextField.layer.masksToBounds = true
-        
-        confirmPasswordTextField.placeholder = "Confirme a senha"
-        confirmPasswordTextField.layer.cornerRadius = 10
-        confirmPasswordTextField.layer.masksToBounds = true
-        
+        textFieldPattern(TextField: nameTextField, Placeholder: "Nome", Radius: 10, maskToBounds: true)
+        textFieldPattern(TextField: lastNameTextField, Placeholder: "Sobrenome", Radius: 10, maskToBounds: true)
+        textFieldPattern(TextField: emailTextField, Placeholder: "Email", Radius: 10, maskToBounds: true)
+        textFieldPattern(TextField: passwordTextField, Placeholder: "Senha", Radius: 10, maskToBounds: true)
+        textFieldPattern(TextField: confirmPasswordTextField, Placeholder: "Confirme a senha", Radius: 10, maskToBounds: true)
     }
     
     func configView() {
-        view.backgroundColor = UIColor(red: 171/255, green: 130/255, blue: 98/255, alpha: 1.0)
+        view.backgroundColor = UIColor.primary
     }
     
     func configLabel() {
@@ -86,9 +68,9 @@ class RegisterViewController: UIViewController {
     }
     
     func configButton() {
-        createAccountButton.backgroundColor = UIColor(red: 243/255, green: 234/255, blue: 222/255, alpha: 1.0)
+        createAccountButton.backgroundColor = UIColor.secondary
         createAccountButton.layer.cornerRadius = 10
-        createAccountButton.tintColor = UIColor(red: 121/255, green: 90/255, blue: 69/255, alpha: 1.0)
+        createAccountButton.tintColor = UIColor.TextPrimary
         createAccountButton.setTitle("Criar conta", for: .normal)
         createAccountButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
         
