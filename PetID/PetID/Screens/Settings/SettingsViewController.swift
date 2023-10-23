e16ff4e1ba9093676513a747092f01c89b3af79b
Fernando Mors <>
2023-10-23 17:27:01 -0400

    refatorei

--- PetID/PetID/Screens/Settings/SettingsViewController.swift
+++ PetID/PetID/Screens/Settings/SettingsViewController.swift
@@ -27,49 +27,43 @@ class SettingsViewController: UIViewController {
         configButton()
     }
     
-    func configView() {
-        view.backgroundColor = UIColor(red: 243/255, green: 234/255, blue: 222/255, alpha: 1.0)
+    private func configView() {
+        view.backgroundColor = UIColor.secondary
         
         
-        viewSettingsView.backgroundColor = UIColor(red: 171/255, green: 130/255, blue: 98/255, alpha: 1.0)
+        viewSettingsView.backgroundColor = UIColor.primary
         viewSettingsView.roundCorners(cornerRadiuns: 30, typeCorners: [.InferiorEsquerdo, .InferiorDireito])
     }
     
-    func configImage() {
+    private func configImage() {
         imagePersonButton.setImage(UIImage(named: "PersonProfile"), for: .normal)
         imagePersonButton.imageView?.sizeThatFits(CGSize.init(width: 100, height: 100))
         
         logoImage.image = UIImage(named: "LogoP")
     }
-    
-    func configLabel() {
-        namePersonLabel.text = "Fernando Mors"
-        namePersonLabel.textColor = .white
-        namePersonLabel.font = UIFont.systemFont(ofSize: 18, weight: .bold)
-        
-        SettingsLabel.text = "Ajustes"
-        SettingsLabel.textColor = .white
-        SettingsLabel.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+
+    private func labelPattern(label: UILabel, text: String, font: UIFont, color: UIColor) {
+        label.text = text
+        label.font = font
+        label.textColor = color
+    }
+
+    private func configLabel() {
+        labelPattern(label: namePersonLabel, text: "Fernando Mors", font: UIFont.systemFont(ofSize: 18, weight: .bold), color: .white)
+        labelPattern(label: SettingsLabel, text: "Ajustes", font: UIFont.systemFont(ofSize: 20, weight: .bold), color: .white)
     }
     
+    private func buttonPattern(button: UIButton, title: String, backgraund: UIColor, tintcolor: UIColor, radius: CGFloat) {
+        button.setTitle(title, for: .normal)
+        button.backgroundColor = backgraund
+        button.tintColor = tintcolor
+        button.layer.cornerRadius = radius
+    }
     
-    
-    func configButton() {
-        editProfileButton.setTitle("Editar perfil", for: .normal)
-        editProfileButton.backgroundColor = UIColor.white
-        editProfileButton.tintColor = UIColor(red: 121/255, green: 90/255, blue: 69/255, alpha: 1.0)
-        editProfileButton.layer.cornerRadius = 10
-        
-        changePasswordButton.setTitle("Trocar senha", for: .normal)
-        changePasswordButton.backgroundColor = UIColor.white
-        changePasswordButton.tintColor = UIColor(red: 121/255, green: 90/255, blue: 69/255, alpha: 1.0)
-        changePasswordButton.layer.cornerRadius = 10
-        
-        myPetsButton.setTitle("Meus pets", for: .normal)
-        myPetsButton.backgroundColor = UIColor.white
-        myPetsButton.tintColor = UIColor(red: 121/255, green: 90/255, blue: 69/255, alpha: 1.0)
-        myPetsButton.layer.cornerRadius = 10
-        
+    private func configButton() {
+        buttonPattern(button: editProfileButton, title: "Editar perfil", backgraund: UIColor.white, tintcolor: UIColor.TextPrimary, radius: 10)
+        buttonPattern(button: changePasswordButton, title: "Trocar senha", backgraund: UIColor.white, tintcolor: UIColor.TextPrimary, radius: 10)
+        buttonPattern(button: myPetsButton, title: "Meus pets", backgraund: UIColor.white, tintcolor: UIColor.TextPrimary, radius: 10)
     }
     
 }
